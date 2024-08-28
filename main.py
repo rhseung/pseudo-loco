499340958a18462221ad137cb1ec017533b05755
Hyunseung Ryu <rhseungg@gmail.com>
2024-08-28 22:17:58 +0900

    .

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import send
 def main():
     send.init()
     message = send.TextMessage()
-    room = "카봇커 봇 분양/실험방"
-    message.text("Hello World!").mention("헤겔").text("dufjaud")
+    room = "방"
+    message.text("Hello World!").mention("류현승").text("ㅁㄴㅇㄹ")
     send.send_message(room, message)
-    send.send_file(room, "17527-Article Text-21021-1-2-20210518.pdf")
+    send.send_file(room, "D:\\Repo\\pseudo-loco\\timetable.png")
     send.send_message(room, send.TextMessage().text("테스트2").lw())
     send.send_message(room, send.TextMessage().text("테스트3"))
-    send.send_file(room, "main.py")
+    send.send_file(room, "D:\\Repo\\pseudo-loco\\requirements.txt")
 
 
 if __name__ == '__main__':
